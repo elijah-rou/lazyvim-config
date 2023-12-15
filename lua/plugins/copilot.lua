60044320303dc797f23bb250febd791bb9b4dc8a
Elijah Roussos <elijah.rou@gmail.com>
2023-12-15 09:52:04 +0200

    commit lazy config

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -12,6 +12,11 @@ return {
       ["c++"] = true,
       ["go"] = true,
       ["python"] = true,
+      ["astro"] = true,
+      ["svelte"] = true,
+      ["clojure"] = true,
+      ["elixir"] = true,
+      ["zig"] = true,
     }
     vim.g.copilot_no_tab_map = true
     vim.api.nvim_set_keymap("i", "<C-m>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
