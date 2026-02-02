01449e5be6d1bbe55a9ecefaa44913d112f5f8d8
Elijah Roussos <elijah.rou@gmail.com>
2026-02-02 14:18:11 -0500

    replace supermaven with augment.vim

---
+++ lua/plugins/augment.lua
@@ -0,0 +1,7 @@
+-- Augment: AI inline code completions
+-- Requires Node.js 22+ and account at augmentcode.com
+-- Tab to accept completions
+return {
+  "augmentcode/augment.vim",
+  event = "InsertEnter",
+}
